bcd28ed76e59b0b290f853b99b23ce688046650f
Sebastián Estrella <sebas007estrella@gmail.com>
2016-07-06 12:21:38 -0500

    Adding tasks with a form

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,22 @@ import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  }
+})
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    target.text.value = '';
   }
 });
