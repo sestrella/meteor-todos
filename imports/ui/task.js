daa82c43d699798d6b23e5322f8b987de66091f0
Sebastián Estrella <sebas007estrella@gmail.com>
2016-07-06 13:27:44 -0500

    Security with methods

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -6,12 +6,10 @@ import './task.html';
 
 Template.task.events({
   'click .toggle-checked'() {
-    Tasks.update(this._id, {
-      $set: { checked: !this.checked }
-    });
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
 
   'click .delete'() {
-    Tasks.remove(this._id);
+    Meteor.call('tasks.remove', this._id);
   }
 });
